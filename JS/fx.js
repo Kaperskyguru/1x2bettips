696780abab05949a871be9f626b4c833f363bbcd
Solomon Eseme <solomoneseme@gmail.com>
2018-07-26 13:09:38 +0100

    Initial commint

---
+++ JS/fx.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $('#menu_icon').click(function(){
+       $('#menu nav').fadeToggle(2000); 
+    });
+    $('#submit2').click(function(){
+       alert("Sent") ;
+    });
+});
